270ed5c0f5ae7cf0a209c8f9641267d8b1574121
gaurav <gauravdhingra9999@gmail.com>
2020-01-19 19:55:41 +0530

    adding files

---
+++ src/main/java/com/example/demo/SpringBootRabbitMqApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitMqApplication.class, args);
+	}
+
+}
